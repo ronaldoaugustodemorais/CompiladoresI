2171ba959944c9db3dc4d0bbfdb2e3c41fc63d7a
Ronaldo Augusto de Morais Sobrinho <ronaldoaugustodemorais@gmail.com>
2018-12-12 08:16:12 -0300

    edit ReaderWrite
    arquivos de leitura e escrita atualizados

---
+++ lerArquivo.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.swing.JOptionPane;
+ 
+public class lerArquivo
+{
+ 
+  public static void main(String[] args)
+  {
+    Scanner ler = new Scanner(System.in);
+	String strArqLeitura = null;
+    String strArqLeitura = JOptionPane.showInputDialog(null,"Onde o arquivo esta localizado?"); //INFORMAR NOME DO ARQUIVO A SER LIDO
+    
+ 
+    System.out.printf("\nConteúdo do arquivo texto:\n");
+    try
+	{
+      FileReader arq = new FileReader(strArqLeitura);
+      BufferedReader lerArq = new BufferedReader(arq);
+ 
+      String linha = lerArq.readLine();
+
+      while (linha != null)
+	  {
+        System.out.printf("%s\n", linha);
+ 
+        linha = lerArq.readLine(); // lê da segunda até a última linha
+      }
+ 
+      arq.close();
+    } catch (IOException e)
+		{
+        System.err.printf("Erro na abertura do arquivo: %s.\n",
+          e.getMessage());
+		}
+ 
+    System.out.println();
+  }
+}
